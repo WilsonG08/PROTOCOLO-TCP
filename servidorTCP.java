42c65a4b6bf4fa0818a2d5c084944a70d9809ce9
WilsonG08 <wilson.guayanay@epn.edu.ec>
2023-11-27 16:22:24 -0500

    si

--- servidorTCP.java
+++ servidorTCP.java
@@ -17,7 +17,7 @@ public class servidorTCP {
                 Socket socket_cliente = socket_servidor.accept();
                 System.out.println("Cliente conectado: "+ socket_cliente.getInetAddress().getHostAddress());
 
-                // Crear un hilo para manejar la conexion con el clientee
+                // Crear un hilo para manejar la conexion con el clienteeeeeee
                 hiloCliente hilo = new hiloCliente(socket_cliente);
                 hilo.start();
             }
